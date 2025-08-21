1c4d99513ca94c77223b97c0c7dd47bc439aeb5f
Skullfire <onlyforworkplsremember@gmail.com>
2025-08-21 17:45:25 +0530

    I know I am a bit late, but made progress on Uno

--- Learning Concepts/Main.java
+++ Learning Concepts/Main.java
@@ -3,45 +3,51 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World !");
+//        System.out.println("Hello World !");
+//
+//        //basic stuff
+//        String name = "";
+//        int age;
+//
+//        Scanner scanner = new Scanner(System.in);
+//        while(true) {
+//            System.out.print("What's you name? :");
+//            name = scanner.nextLine();
+//            if (name.length() > 0) {
+//                break;
+//            } else {
+//                System.out.println("Please enter something .");
+//            }
+//        }
+//
+//        while(true){
+//            System.out.println("And how old are you ? :");
+//            try {
+//                age = scanner.nextInt();
+//                break;
+//            }catch (InputMismatchException e){
+//                System.out.println("Please only enter numeric values .");
+//                scanner.nextLine();
+//            }
+//        }
+//
+//        System.out.println("Hello there " + name);
+//        System.out.println("So you are " + age + " years old huh.");
+//        if(age > 18){
+//            System.out.println(
+//                    "Well you an adult, you don't need my advice ."
+//            );
+//        }else{
+//            System.out.println("Since you are still young, let me give you some advice pal " +
+//                    "Life is like something in your pants " +
+//                    "sometimes its hard, but it is never hard forever" +
+//                    " Sayonara.");
+//        }
+//
+//        Rps n = new Rps(3);
+//        n.run();
 
-        //basic stuff
-        String name = "";
-        int age;
-
-        Scanner scanner = new Scanner(System.in);
-        while(true) {
-            System.out.print("What's you name? :");
-            name = scanner.nextLine();
-            if (name.length() > 0) {
-                break;
-            } else {
-                System.out.println("Please enter something .");
-            }
-        }
-
-        while(true){
-            System.out.println("And how old are you ? :");
-            try {
-                age = scanner.nextInt();
-                break;
-            }catch (InputMismatchException e){
-                System.out.println("Please only enter numeric values .");
-                scanner.nextLine();
-            }
-        }
-
-        System.out.println("Hello there " + name);
-        System.out.println("So you are " + age + " years old huh.");
-        if(age > 18){
-            System.out.println(
-                    "Well you an adult, you don't need my advice ."
-            );
-        }else{
-            System.out.println("Since you are still young, let me give you some advice pal " +
-                    "Life is like something in your pants " +
-                    "sometimes its hard, but it is never hard forever" +
-                    " Sayonara.");
-        }
+        Uno u = new Uno();
+        u.start();
     }
 }
--- Learning Concepts/Uno.java
+++ Learning Concepts/Uno.java
@@ -1,52 +1,114 @@
-import java.util.Random;
+import java.util.*;
 
 public class Uno {
 
-    String[] cards = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "Reverse", "Skip", "+2"};
-    String[] colors = {"Red", "Blue", "Green", "Yellow"};
-    String[] specialCards = {"+4", "Color Change"};
-    int num_cards = (cards.length * colors.length * 2) + specialCards.length * 2;
-    String[] deck;
-    Random random;
+    private String[] cards = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "Reverse", "Skip", "+2"};
+    private String[] colors = {"Red", "Blue", "Green", "Yellow"};
+    private String[] specialCards = {"+4", "Color Change"};
+    private final int num_cards = (cards.length * colors.length * 2) + specialCards.length * 2;
+    private final int start_cards = 7;
+    private String[][] deck;
+    private  Random random;
+    private Scanner scanner;
 
-    String[] playerInven;
-    String[] compInven;
+    List<String[]> playerInven;
+    List<String[]> compInven;
+    String[] top_card;
 
     Uno(){
-        this.deck = new String[num_cards];
+        this.deck = new String[num_cards][2];
         this.random = new Random();
+        this.scanner = new Scanner(System.in);
+        this.top_card = new String[2];
+        this.playerInven = new ArrayList<>();
+        this.compInven = new ArrayList<>();
         this.genDeck();
-
+        this.getTopCard();
     }
 
-    String[] getCard(int numOfCards){
-        String[] output = new String[numOfCards];
+    public void start(){
+        System.out.println("Starting game...");
+        System.out.println("Distributing Cards...");
+        this.addCards(playerInven, start_cards);
+        this.addCards(compInven, start_cards);
+        while(!playerInven.isEmpty() && !compInven.isEmpty()){
+            this.getPlayerInput();
+        }
+    }
+
+    private void getTopCard(){
+        int temp;
+        while(true) {
+            temp = this.random.nextInt(0, this.deck.length);
+            if(this.deck[temp][0] != null) {
+                this.top_card[0] = this.deck[temp][0];
+                this.top_card[1] = this.deck[temp][1];
+                break;
+            }
+        }
+    }
+
+    private void addCards(List<String[]> inven, int num){
+        inven.addAll(this.getCards(num));
+    }
+
+    private void showPlayerInven(){
+        for(int i = 0; i < playerInven.size(); ++i){
+            System.out.println((i + 1) + " " + this.playerInven.get(i)[0] + " " + this.playerInven.get(i)[1]);
+        }
+    }
+
+    private void getPlayerInput(){
+        while(true){
+            this.showPlayerInven();
+            System.out.println("Enter the index of the card you want to use.");
+            System.out.println("Enter 100 if you want to draw a card.");
+            try{
+                int choice = this.scanner.nextInt();
+                if(0 < choice && choice <= playerInven.size()){
+                    break;
+                }else{
+                    System.out.println("Please only enter valid index.");
+                }
+            }catch (InputMismatchException e){
+                System.out.println("Invalid input !");
+            }
+        }
+    }
+
+    private List<String[]> getCards(int numOfCards){
+        List<String[]> output = new ArrayList<>();
         for(int i = 0; i < numOfCards; ++i){
             while(true) {
                 int temp = random.nextInt(0, this.deck.length);
-                String chosen = this.deck[temp];
-                if(!chosen.equals(" ")) {
-                    output[i] = this.deck[temp];
-                    this.deck[temp] = "";
+                String[] chosen = this.deck[temp];
+                if(!chosen[1].isEmpty()) {
+                    output.add(this.deck[temp]);
+                    this.deck[temp][1] = "";
                     break;
                 }
             }
         }
+        for(String[] card: output){
+            System.out.println(card[0] + card[1]);
+        }
         return output;
     }
 
-    void genDeck(){
+    private void genDeck(){
         int count = 0;
         for(String card : cards){
             for(String color : colors){
-                this.deck[count] = color + " " + card;
-                this.deck[count + 1] = color + " " + card;
+                this.deck[count][0] = color;
+                this.deck[count][1] = card;
+                this.deck[count + 1][0] = color;
+                this.deck[count + 1][1] = card;
                 count += 2;
             }
         }
         for(String card : specialCards){
-            this.deck[count] = card;
-            this.deck[count + 1] = card;
+            this.deck[count][1] = card;
+            this.deck[count + 1][1] = card;
             count += 2;
         }
     }
