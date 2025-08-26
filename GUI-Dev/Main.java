29b5eb3e78202d5c776e0a442e9b0e4eef0825a8
Skullfire <onlyforworkplsremember@gmail.com>
2025-08-26 10:45:31 +0530

    Started Learning GUI Dev

---
+++ GUI-Dev/Main.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.lang.classfile.instruction.OperatorInstruction;
+
+// this marks the start of GUI Dev in Java
+// we use the java swing library for GUI Dev and also Game Dev
+public class Main {
+    public static void main(String[] args){
+        JFrame frame = new JFrame(); // creates a window
+        frame.setSize(500, 500); // sets a window size
+        frame.setTitle("My window"); // sets the title of the window
+        frame.setVisible(true); // sets visibility of the window to true
+        frame.setResizable(false); // prevents frame from being resized
+        frame.setLayout(null);
+
+        Border border = BorderFactory.createLineBorder(Color.WHITE);
+
+        //labels
+        JLabel label = new JLabel();
+        label.setText("This is a label");
+        label.setForeground(new Color(0xFFFFFF));
+        label.setFont(new Font("Cascadia Mono", Font.PLAIN, 20));
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setBounds(100, 100, 250, 250);
+
+        //panels (or frames in python context)
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.RED);
+        redPanel.setBounds(0, 0, 100,100);
+        redPanel.setLayout(new BorderLayout());
+
+        frame.add(label);
+        frame.add(redPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application, without this the program doesn't end when x button is clicked
+        frame.getContentPane().setBackground(Color.BLACK);
+
+        //binding events to the frame
+        frame.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                System.out.println("The key " + e.getKeyChar() + " was pressed.");
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                System.out.println("The key " + e.getKeyChar() + " was released.");
+            }
+        });
+    }
+}
